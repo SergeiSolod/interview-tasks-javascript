10daef8834545fdc12f5e1d49007f480a04076ba
SergeiSolod <solodsergey287@gmail.com>
2024-03-30 10:45:22 +0300

    add-two-promises (2)

--- leetcode/30-days-of-javascript/12_add-two-promises.js
+++ leetcode/30-days-of-javascript/12_add-two-promises.js
@@ -3,3 +3,9 @@ let addTwoPromises = async (promise1, promise2) => {
     let theRes2 = await promise2;
     return (theRes1 + theRes2);
 }
+
+let addTwoPromises2 = (
+    (promise1, promise2) => (
+        Promise.all([promise1, promise2]).then((theRes)=>(theRes[0]+theRes[1]))
+    )
+)
