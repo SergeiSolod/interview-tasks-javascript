3cc2973a425ce65136fc83abc73686a00cab0ced
SergeiSolod <solodsergey287@gmail.com>
2024-04-01 18:46:07 +0300

    promise-time-limit

--- leetcode/30-days-of-javascript/16_promise-time-limit.js
+++ leetcode/30-days-of-javascript/16_promise-time-limit.js
@@ -1,5 +1,22 @@
 const timeLimit = (fn, t) => {
     return function (...args) {
+        const p1 = fn(...args)
+        const p2 = new Promise(
+            (doRes, doRej)=> {
+                setTimeout(
+                    () => doRej("Time Limimit Exceeded"), t
+                )
+            }
+        );
+
+        return Promise.race(
+            [p1, p2]
+        )
+    }
+}
+
+const timeLimit2 = (fn, t) => {
+    return function (...args) {
         let thePromise = (
             new Promise(
                 (doRes, doRej) => {
